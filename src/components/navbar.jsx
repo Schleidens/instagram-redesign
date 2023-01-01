d422f0e1d30e3be8de2aa124612f16c78f2fdf66
Schleidens <alectineschleidens2016@gmail.com>
2022-12-31 23:07:28 -0500

    basically create the navbar and print it

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div className='flex justify-between'>
+        <div>i</div>
+        <div>f</div>
+        <div>g</div>
+    </div>
+  )
+}
