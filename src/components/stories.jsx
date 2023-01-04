ed621c815a7d909137073bff78cef77b89fc917a
Schleidens <alectineschleidens2016@gmail.com>
2023-01-03 23:31:17 -0500

    basic design of story compo

--- src/components/stories.jsx
+++ src/components/stories.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import stories from  '../data/stories'
 
-export default function stories() {
+export default function storiesCompo() {
   return (
-    <div>
-        ok
+    <div className="flex">
+        <div className='flex justify-center overflow-x-scrool'>
+       {stories.map((story, index) =>(
+        <div className="rounded-full mx-2" key={index}>
+            <img src={story.img} className="rounded-full h-20 w-20" alt="" />
+            <span className='text-sm'>
+                {story.username}
+            </span>
+        </div>
+       ))}
+        </div>
+        <div className="ok">
+            view more
+        </div>
     </div>
   )
 }
