e1935b0158e01d328b4b2544f3a31c46b1b07be3
Schleidens <alectineschleidens2016@gmail.com>
2023-01-04 13:25:39 -0500

    make it look awesome

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stories from './stories'
+import Post from './post'
 
 export default function mainCompo() {
   return (
-    <div className="mt-6 mx-2 md:mx-42 lg:mx-52">
+    <div className="mt-6 mx-2 md:mx-20 lg:mx-96">
         <Stories />
+        <Post />
     </div>
   )
 }
