2c4ec5d85b9f9874240762dcc8267ce15636090c
Schleidens <alectineschleidens2016@gmail.com>
2023-01-03 23:32:11 -0500

    added stories data fr list

---
+++ src/data/stories.js
@@ -0,0 +1,33 @@
+const stories = [
+    {
+        "username": "juststeve",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "username": "youcandev",
+        "img": "https://images.unsplash.com/photo-1635792934018-ca57843086ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    },
+    {
+        "username": "youcandev",
+        "img": "https://images.unsplash.com/photo-1635792934018-ca57843086ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    },
+    {
+        "username": "youcandev",
+        "img": "https://images.unsplash.com/photo-1635792934018-ca57843086ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    },
+    {
+        "username": "youcandev",
+        "img": "https://images.unsplash.com/photo-1635792934018-ca57843086ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    },
+    {
+        "username": "youcandev",
+        "img": "https://images.unsplash.com/photo-1635792934018-ca57843086ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    },
+    {
+        "username": "youcandev",
+        "img": "https://images.unsplash.com/photo-1635792934018-ca57843086ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    }
+    
+]
+
+export default stories;
