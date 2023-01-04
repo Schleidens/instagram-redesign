976358eb9a57294143d413c9fbe9d9c3b6936f0d
Schleidens <alectineschleidens2016@gmail.com>
2023-01-04 13:55:07 -0500

    added data for post list

---
+++ src/data/posts.js
@@ -0,0 +1,46 @@
+const posts = [
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "okayBaby",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    },
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "Johny",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    },
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "tellMeTheTruth",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    },
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "maboy",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    },
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "youreDumb",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    },
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "letssgo",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    },
+    {
+        "profile": "https://images.unsplash.com/photo-1671040605774-f06cd4a8ba38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "username": "notAUsername",
+        "img": "https://images.unsplash.com/photo-1630393213394-b1bb4c99b225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "caption": "look at this dude !!!"
+    }
+]
+
+export default posts;
