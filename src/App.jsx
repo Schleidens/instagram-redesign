0b5ee28c030c94931e4e63f5c5788e78e7f7c716
Schleidens <alectineschleidens2016@gmail.com>
2023-01-03 22:03:35 -0500

    print stories compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/navbar"
+import Stories from "./components/stories"
 
 function App() {
 
   return (
     <div className="text-2xl">
      <Navbar />
+     <div className="mt-6 mx-52">
+       <Stories />
+     </div>
     </div>
   )
 }
