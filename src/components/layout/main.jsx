37defbc7a24229573b11564fd239eb2c1bb2802c
Schleidens <alectineschleidens2016@gmail.com>
2023-01-07 21:09:21 -0500

    add the newpost button to the main page

--- src/components/layout/main.jsx
+++ src/components/layout/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Stories from '../stories'
 import Post from '../post'
+import NewPost from '../newPost'
 
 export default function mainCompo() {
   return (
     <div className="mt-6 mx-2 md:mx-20 lg:mx-96">
         <Stories />
         <Post />
+        <NewPost />
     </div>
   )
 }
