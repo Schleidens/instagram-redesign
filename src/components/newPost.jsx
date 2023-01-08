35bb7277b81af0828c0c40629f9147f1b78ce321
Schleidens <alectineschleidens2016@gmail.com>
2023-01-07 21:08:20 -0500

    desgin th new post button

--- src/components/newPost.jsx
+++ src/components/newPost.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BsPlusLg } from 'react-icons/bs'
 
 export default function newPost() {
   return (
-    <div>
-        new post
+    <div className='fixed bottom-20 right-60 text-3xl hidden lg:block'>
+      <button className="new__btn">
+        <BsPlusLg className='btn__svg' />
+      </button>
     </div>
   )
 }
