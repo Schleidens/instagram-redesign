9839f89aed111da2bb4ffc311c8857d448d2a2d8
Schleidens <alectineschleidens2016@gmail.com>
2023-01-08 19:42:49 -0500

    create footer

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <div>
+        <div className="flex justify-center text-center py-4 ml-1 text-gray-700">
+            Build with  &#9829; by <a href="https://github.com/Schleidens" className='ml-1 underline' target="blank"> Schleidens.Dev</a>
+        </div>
+    </div>
+  )
+}
